07f714d4eaf80fc76b711528d7aeacf3dbe292df
Monty Rasmussen <montyr75@gmail.com>
2025-07-30 19:28:15 -0600

    version bump

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -4,7 +4,7 @@ import 'features/app/services/logger_service.dart';
 
 // app info
 const appName = "bowling_bingo";
-const version = "0.0.6";
+const version = "0.0.7";
 const debugMode = !kReleaseMode;
 
 // create logger
